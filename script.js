6a5dfcb8493315b311db74181c4723e1a3d01802
Pavel Verescako <P.Verescako@gmail.com>
2020-11-20 01:55:19 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+    let money,
+        income, 
+        addExpenses, 
+        deposit, 
+        mission, 
+        period;
+    
+    alert('лучше поздно, чем никогда');
+    console.log('Из всего, что я потерял, мне больше всего не хватает мозгов…');
